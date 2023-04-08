cf2fb887682ccf4a30c06875a8f3cb8f046ad09c
aki <lala.akira@gmail.com>
2023-04-09 06:42:22 +0900

    fix: Support for vite_ruby

--- lib/static_resources_rails.rb
+++ lib/static_resources_rails.rb
@@ -15,6 +15,11 @@ module StaticResourcesRails
         asset_host = ->(source) { %r|\A/#{Regexp.union(aseet_dirs)}/|.match?(source) ? bucket_host : nil }
         Rails.application.config.action_controller.asset_host = asset_host
         Rails.application.config.action_mailer.asset_host = asset_host
+
+        # SEE: https://github.com/ElMassimo/vite_ruby/pull/203
+        if defined?(ViteRuby)
+          ENV['VITE_RUBY_ASSET_HOST'] = bucket_host
+        end
       end
       Rails.application.config.assets.manifest = "public/assets/#{sprockets_manifest_filename}"
     end
