8a0b4e0fed352390b22696abd9a4a7fc560096ec
aki <lala.akira@gmail.com>
2023-04-10 15:05:02 +0900

    perf: 正規表現オブジェクトを都度生成しないように

--- lib/static_resources_rails.rb
+++ lib/static_resources_rails.rb
@@ -7,12 +7,13 @@ module StaticResourcesRails
   class SyncError < Error; end
 
   class << self
-    attr_accessor :region, :sprockets_manifest_filename, :additional_sync_dirs, :additional_manifest_files
+    attr_accessor :region, :sprockets_manifest_filename, :additional_manifest_files
+    attr_reader :additional_sync_dirs
 
     def set_bucket(value, with_asset_host: true)
       @bucket = value
       if with_asset_host
-        asset_host = ->(source) { %r|\A/#{Regexp.union(aseet_dirs)}/|.match?(source) ? bucket_host : nil }
+        asset_host = ->(source) { asset_host_pattern.match?(source) ? bucket_host : nil }
         Rails.application.config.action_controller.asset_host = asset_host
         Rails.application.config.action_mailer.asset_host = asset_host
 
@@ -36,6 +37,15 @@ module StaticResourcesRails
 
     def aseet_dirs
       ['assets', *additional_sync_dirs]
+    end
+
+    def asset_host_pattern
+      @asset_host_pattern ||= %r|\A/#{Regexp.union(aseet_dirs)}/|
+    end
+
+    def additional_sync_dirs=(value)
+      @additional_sync_dirs = value
+      @asset_host_pattern = nil
     end
   end
 
